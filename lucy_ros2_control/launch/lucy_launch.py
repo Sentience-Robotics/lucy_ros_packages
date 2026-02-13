6f0e945f5108555174bc16fd8b105c5ad7e52d94
sambrus <samuel.bruschet@epitech.eu>
2026-02-13 20:21:34 +0100

    wip(urdf): updating joint names for ros2_control

--- lucy_ros2_control/launch/lucy_launch.py
+++ lucy_ros2_control/launch/lucy_launch.py
@@ -1,21 +1,69 @@
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-from launch.actions import TimerAction
+from launch.substitutions import Command, PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
 import os
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    urdf_path = '/home/dev/thais_urdf/InMoov/urdf/InMoov.urdf.xacro'
+    # controllers_yaml = '/home/dev/lucy_ws/src/lucy_ros_packages/lucy_ros2_control/config/lucy_controllers.yaml'
+    # urdf_path = '/home/samuel/sentience/thais_urdf/inmoov/urdf/inmoov.urdf.xacro'
+    urdf_path = '/home/samuel/sentience/local_ws/src/thais_urdf/inmoov/urdf/inmoov.urdf.xacro'
+    # pkg_gazebo = get_package_share_directory('gazebo_ros')
 
-    controllers_yaml = '/home/dev/lucy_ws/src/lucy_ros_packages/lucy_ros2_control/config/lucy_controllers.yaml'
+    # Lancement de Gazebo
+    # gazebo = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(pkg_gazebo, 'launch', 'gazebo.launch.py')
+    #     )
+    # )
 
-    # controllers_yaml = PathJoinSubstitution([
-        # FindPackageShare('lucy_ros2_control'),
-        # 'config',
-        # 'lucy_controllers.yaml'
+    # Publie le robot_description
+    # robot_state_publisher = Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     output='screen',
+    #     parameters=[{'robot_description': Command(['xacro ', urdf_path])}]
+    # )
+
+    # # Spawn le robot dans Gazebo après 20 secondes
+    # spawn_entity = TimerAction(
+    #     period=20.0,  # délai en secondes
+    #     actions=[
+    #         Node(
+    #             package='gazebo_ros',
+    #             executable='spawn_entity.py',
+    #             arguments=[
+    #             '-topic', 'robot_description',
+    #             '-entity', 'mon_robot',
+    #             '-x', '0', '-y', '0', '-z', '0.5'  # sur le sol
+    #             ],
+    #             output='screen'
+    #         )
+    #     ]
+    # )
+
+    # return LaunchDescription([
+    #     gazebo,
+    #     robot_state_publisher,
+    #     spawn_entity
     # ])
+
+
+
+    controllers_yaml = PathJoinSubstitution([
+        FindPackageShare('lucy_ros2_control'),
+        'config',
+        'lucy_controllers.yaml'
+    ])
+
+    robot_description_deprecated = {
+    'robot_description': Command(['xacro ', urdf_path])
+    }
+
 
     ros2_control_node = TimerAction(
         period=10.0,
@@ -23,7 +71,7 @@ def generate_launch_description():
             package='controller_manager',
             executable='ros2_control_node',
             output='screen',
-            parameters=[controllers_yaml]
+            parameters=[controllers_yaml, robot_description_deprecated]
         )]
     )
 
