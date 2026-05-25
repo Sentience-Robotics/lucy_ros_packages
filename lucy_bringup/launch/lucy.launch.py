99db27e38a0156ef952788cdf55a254d96334b8a
Charles Madjeri <80175305+charlesmadjeri@users.noreply.github.com>
2026-05-25 17:15:48 +0200

    fix(bringup): wrap long launch import for flake8 E501

--- lucy_bringup/launch/lucy.launch.py
+++ lucy_bringup/launch/lucy.launch.py
@@ -47,7 +47,12 @@ from launch.actions import (
 )
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command, PythonExpression
+from launch.substitutions import (
+    Command,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+    PythonExpression,
+)
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
